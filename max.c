eaf457a2193182ebd284025bb5b832c8f57bbed9
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-11-06 17:18:19 +0100

    Update max.c

--- max.c
+++ max.c
@@ -5,7 +5,25 @@
  */
 
 
-int max(int a, int b)
+
+int main()
+{
+     int x,y;
+
+    printf("Enter the two num to check max and min: ");
+    scanf("%d %d", &x, &y);
+
+    printf("The maximum num is %d", max(x,y));
+    putchar('\n');
+     printf("The minimum num is %d", min(x,y));
+
+    return 0;
+}
+   
+
+
+//Function to calculate the max num
+  int max(int a, int b)
 {
     if(a > b)
     {
@@ -15,6 +33,9 @@ int max(int a, int b)
         return b;
     }
 }
+
+//Function to calculate the min num
+
 int min(int a, int b)
 {
     if(a<b)
@@ -25,12 +46,3 @@ int min(int a, int b)
         return b;
     }
 }
-
-int main()
-{
-    int x,y;
-
-    printf("Enter the two num to check max and min: ");
-    scanf("%d %d", &x, &y);
-
-    printf("The maximum num is %d"
